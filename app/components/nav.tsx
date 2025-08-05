ccb4de1e469d8954f96bd5189306c86638a12b01
SHAIK JAVID <shaikameena375@gmail.com>
2025-08-05 21:03:59 +0530

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,19 +4,15 @@ export default function Nav() {
   return (
     <header className="mb-16">
       <nav className="flex justify-between items-center py-10">
-        <Link href="/" className="text-2xl font-bold text-black dark:text-white">
-          Shaik Javid
+        <Link href="/">
+          <span className="text-2xl font-bold text-black dark:text-white">
+            Shaik Javid
+          </span>
         </Link>
         <div className="space-x-4 text-neutral-600 dark:text-neutral-300 text-sm">
-          <Link href="#about" className="hover:underline">
-            About
-          </Link>
-          <Link href="#projects" className="hover:underline">
-            Projects
-          </Link>
-          <Link href="#contact" className="hover:underline">
-            Contact
-          </Link>
+          <a href="#about" className="hover:underline">About</a>
+          <a href="#projects" className="hover:underline">Projects</a>
+          <a href="#contact" className="hover:underline">Contact</a>
         </div>
       </nav>
     </header>
